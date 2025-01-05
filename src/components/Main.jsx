117fd48d28bd1cfce6c93a837622fa98b1a94832
menth0s <marslite@pm.me>
2025-01-05 07:46:21 +0100

    Implemented new fonts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,9 @@
+import "./Main.css";
 function Main() {
   return (
-    <div>
+    <div className="container">
       <p>This is your fist Vite project lol</p>
+      <p> "Vite" means "quick" in French</p>
     </div>
   );
 }
